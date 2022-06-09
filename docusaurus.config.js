679c3b6c5063f89d1a311ac2e4c0c0141176d00c
luke <luke.belliveau@gmail.com>
2022-06-09 16:42:53 -0400

    remove broken stuff so app will build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,60 +60,60 @@ const config = {
           alt: "My Site Logo",
           src: "img/logo.svg",
         },
-        items: [
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/lukebelliveau/",
-            label: "My GitHub",
-            position: "right",
-          },
-        ],
+        // items: [
+        //   { to: "/blog", label: "Blog", position: "left" },
+        //   {
+        //     href: "https://github.com/lukebelliveau/",
+        //     label: "My GitHub",
+        //     position: "right",
+        //   },
+        // ],
       },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: "dark",
+      //   links: [
+      //     {
+      //       title: "Docs",
+      //       items: [
+      //         {
+      //           label: "Tutorial",
+      //           to: "/docs/intro",
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: "Community",
+      //       items: [
+      //         {
+      //           label: "Stack Overflow",
+      //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+      //         },
+      //         {
+      //           label: "Discord",
+      //           href: "https://discordapp.com/invite/docusaurus",
+      //         },
+      //         {
+      //           label: "Twitter",
+      //           href: "https://twitter.com/docusaurus",
+      //         },
+      //       ],
+      //     },
+      //     // {
+      //     //   title: "More",
+      //     //   items: [
+      //     //     {
+      //     //       label: "Blog",
+      //     //       to: "/blog",
+      //     //     },
+      //     //     {
+      //     //       label: "GitHub",
+      //     //       href: "https://github.com/facebook/docusaurus",
+      //     //     },
+      //     //   ],
+      //     // },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
